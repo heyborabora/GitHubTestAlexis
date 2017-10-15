054bb95686c041aa1f7bfd692af777faf58133a6
heyborabora <heyborabora@gmail.com>
2017-10-15 11:15:07 -0700

    homework4

--- website/app.js
+++ website/app.js
@@ -1,2 +1,24 @@
-var d = new Date();
-document.getElementById("output").innerHTML = d;
+let num = prompt('Please enter a number between 2 and 10:');
+let numCount = parseInt(num, 10);
+
+for (let i = 1;
+    (numCount / 2 ** i) > 0.000001; i++) {
+    lastNum = i + 1;
+}
+//console.log(lastNum);
+
+function getNum() {
+    if (numCount <= 1 || numCount >= 11) {
+        document.getElementById("warning").innerHTML = "Your input is " + numCount + ". The valid input number is between 2 and 10. Please reload this page and try again.";
+    } else {
+        document.getElementById("msg").innerHTML = "Your input number is " + numCount;
+        document.getElementById("msg2").innerHTML = "The number of times to divide " + numCount + " by 2 to get a value less than one millionth is " + lastNum
+    }
+}
+getNum();
+
+
+let arr = ['*', '**', '***', '****', '*****', '******', '*******', '********', '*********', '**********']
+for (let i = numCount; i >= 1; i--) {
+    if (numCount > 10) {} else { document.write('<br>' + arr[i - 1]) }
+}
